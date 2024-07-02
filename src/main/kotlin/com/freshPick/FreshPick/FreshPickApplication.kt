7859e1a2851c578a4659a0e1779cf30310eb9806
AleksanderBanasiak <olek.banasiak0@gmail.com>
2024-07-02 18:43:08 +0200

    new project

---
+++ src/main/kotlin/com/freshPick/FreshPick/FreshPickApplication.kt
@@ -0,0 +1,11 @@
+package com.freshPick.FreshPick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreshPickApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreshPickApplication>(*args)
+}
---
+++ src/test/kotlin/com/freshPick/FreshPick/FreshPickApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.freshPick.FreshPick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreshPickApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
